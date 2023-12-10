558fff78aed8665f1bafc8d5f9c14e36dd680ba8
Abdelghani <143917624+babdellghani@users.noreply.github.com>
2023-12-10 20:00:14 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,15 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=project1';
+    $user = 'root';
+    $pass = '';
+    $options = [
+        PDO::MYSQL_ATTR_COMPRESS => "SET NAMES utf8"
+    ];
 
+    try {
+        $db = new PDO($dsn, $user, $pass, $options);
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+        die();
+    }
+?>
