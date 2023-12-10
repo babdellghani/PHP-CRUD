bfe1f4caeebb2a993e436c9602ecdbeeebfafc25
Abdelghani <143917624+babdellghani@users.noreply.github.com>
2023-12-10 20:01:23 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,12 @@
+<?php
 
+    require_once 'connect.php';
+    $id = $_GET['id'];
+    $state = $db->prepare('DELETE FROM users WHERE id = ?');
+    $delete = $state->execute([$id]);
+    if ($delete) {
+        header('Location: index.php');
+        exit();
+    } else {
+        echo 'Error, Not Deleted';
+    }
